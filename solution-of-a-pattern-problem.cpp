68c7d19bae68093fbb5e04ce592a2bd169a7ce76
IsteakAhmedShupto <isteakahmedshupto@gmail.com>
2022-12-13 01:58:03 +0600

    first commit

---
+++ solution-of-a-pattern-problem.cpp
@@ -0,0 +1,39 @@
+// 1 + (2 + 3 * 4) + (5 + 6 * 7 + 8 * 9 * 10) + .... + nth term
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+ll count1 = 1, count2 = 0;
+
+ll patternSolve(ll count, ll n)
+{
+    ll sum = 0, mul = 1;
+    // ll count = 1;
+
+    for (ll i = 1; i <= n; i++)
+    {
+        mul = 1;
+        for (ll j = 1; j <= i; j++)
+        {
+            mul *= count;
+            count++;
+        }
+        sum += mul;
+    }
+
+    count1 = count;
+    return sum;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    ll n;
+    cin >> n;
+    for (ll i = 1; i <= n; i++)
+        cout << patternSolve(count1, i) << "\n";
+}
